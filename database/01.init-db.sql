819752c49e38038f1df71bfe6b8140427c217bae
Adam Cala <adam.cala31@gmail.com>
2024-07-27 19:53:08 +0200

    basic db schema

--- database/01.init-db.sql
+++ database/01.init-db.sql
@@ -1,5 +1,19 @@
--- CREATE TABLE
-DROP TABLE IF EXISTS test;
-CREATE TABLE test (
+-- CREATE TABLE USERS
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
+    uname TEXT NOT NULL,
+    passwd TEXT NOT NULL
+);
+
+-- CREATE TABLE POSTS
+
+CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    contents TEXT NOT NULL,
+    photoUrl TEXT,
+    CONSTRAINT author
+        FOREIGN KEY (id)
+            REFERENCES users(id)
+            ON DELETE CASCADE
 );
