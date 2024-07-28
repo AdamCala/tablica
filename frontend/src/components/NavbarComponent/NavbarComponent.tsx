72cf67ec7cc523e32ce6c57b9b60dde9c487373d
Adam Cala <adam.cala31@gmail.com>
2024-07-28 19:43:46 +0200

    navbar

--- frontend/src/components/NavbarComponent/NavbarComponent.tsx
+++ frontend/src/components/NavbarComponent/NavbarComponent.tsx
@@ -1,7 +1,14 @@
 import styles from "../../styles/layout/_navbar-component.module.scss";
 
 const NavbarComponent = () => {
-  return <div>NavbarComponent</div>;
+  return (
+    <>
+      <div className={styles.main}>
+        <h1>Tablica</h1>
+      </div>
+      <div className={styles.line} />
+    </>
+  );
 };
 
 export default NavbarComponent;
--- frontend/src/pages/RootPage/RootPage.tsx
+++ frontend/src/pages/RootPage/RootPage.tsx
@@ -1,18 +1,21 @@
+import NavbarComponent from "../../components/NavbarComponent/NavbarComponent";
 import PostComponent from "../../components/PostComponent/PostComponent";
 import styles from "../../styles/pages/_root-page.module.scss";
 
 const RootPage: React.FC = () => {
   return (
     <>
+      <NavbarComponent />
       <div className={styles.main}>
         <div className={styles.posts}>
           <PostComponent image={false} />
-          <PostComponent image={true} />
-          <PostComponent image={true} />
           <PostComponent image={false} />
           <PostComponent image={true} />
           <PostComponent image={false} />
           <PostComponent image={true} />
+          <PostComponent image={false} />
+          <PostComponent image={true} />
+          <PostComponent image={false} />
           <PostComponent image={false} />
         </div>
       </div>
