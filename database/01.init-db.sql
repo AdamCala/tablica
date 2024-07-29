a3a7272d3e6f99e9ee6a28db9652d3ec2c1e3899
Adam Cala <adam.cala31@gmail.com>
2024-07-29 17:43:02 +0200

    migrations for database withn prisma

--- database/01.init-db.sql
+++ database/01.init-db.sql
@@ -1,19 +1,19 @@
--- CREATE TABLE USERS
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    uname TEXT NOT NULL,
-    passwd TEXT NOT NULL
-);
+-- -- CREATE TABLE USERS
+-- CREATE TABLE IF NOT EXISTS users (
+--     id SERIAL PRIMARY KEY,
+--     uname TEXT NOT NULL,
+--     passwd TEXT NOT NULL
+-- );
 
--- CREATE TABLE POSTS
+-- -- CREATE TABLE POSTS
 
-CREATE TABLE IF NOT EXISTS posts (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    contents TEXT NOT NULL,
-    photoUrl TEXT,
-    CONSTRAINT author
-        FOREIGN KEY (id)
-            REFERENCES users(id)
-            ON DELETE CASCADE
-);
+-- CREATE TABLE IF NOT EXISTS posts (
+--     id SERIAL PRIMARY KEY,
+--     title TEXT NOT NULL,
+--     contents TEXT NOT NULL,
+--     photoUrl TEXT,
+--     CONSTRAINT author
+--         FOREIGN KEY (id)
+--             REFERENCES users(id)
+--             ON DELETE CASCADE
+-- );
